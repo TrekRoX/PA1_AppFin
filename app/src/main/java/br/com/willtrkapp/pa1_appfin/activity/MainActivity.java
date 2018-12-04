5dc50f3a7d7f49a3a010124140366653a96ed851
Willian de Lima <will_trk@hotmail.com>
2018-12-03 22:17:14 -0200

    Criando floating menu.

--- app/src/main/java/br/com/willtrkapp/pa1_appfin/activity/MainActivity.java
+++ app/src/main/java/br/com/willtrkapp/pa1_appfin/activity/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import br.com.willtrkapp.pa1_appfin.R;
 import br.com.willtrkapp.pa1_appfin.data.ContaDAO;
 
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
